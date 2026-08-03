5b55ec0970eb60657ed9b157e33c88315d72a58d
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-03 18:26:44 +0300

    feat:configured tailwind

---
+++ src/features/LandingPage.tsx
@@ -0,0 +1,8 @@
+//components
+import Page from "../components/Page"
+
+export default function LandingPage() {
+    return (
+        <Page>LandingPage</Page>
+    )
+}
---
+++ src/features/SplashScreen.tsx
@@ -0,0 +1,8 @@
+//components
+import Page from "../components/Page"
+
+export default function SplashScreen() {
+    return (
+        <Page>SplashScreen</Page>
+    )
+}
