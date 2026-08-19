26a510f9bacf60f46a5c0ed44de8bc47788195a7
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-20 02:27:11 +0300

    feat:added a script for file selection

--- src/scripts/utils/files.ts
+++ src/scripts/utils/files.ts
@@ -1,3 +1,17 @@
 export default class Files {
-    static selectImage() { }
+    static selectImage(): Promise<File | null> {
+        return new Promise((resolve) => {
+            const input = document.createElement("input")
+
+            input.type = "file"
+            input.accept = "image/*"
+
+            input.onchange = () => {
+                const file = input.files?.[0] ?? null
+                resolve(file)
+            }
+
+            input.click()
+        })
+    }
 }
